a892bc3bd0ca31935a21c480d21659d547bb9a73
DESKTOP-F0UGBP9\deakl <deak.lala@hotmail.com>
2020-05-18 22:58:43 +0200

    Build

---
+++ build/precache-manifest.791efe53d3dcf789fe9d50e3872060a0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcf66c79af7b40bc942df0e6587d65e8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "60ec9f85e324c9cd9578",
+    "url": "./static/css/main.c1feb274.chunk.css"
+  },
+  {
+    "revision": "5ed28e986c9d3ccfaaa3",
+    "url": "./static/js/2.0f4066fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.0f4066fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60ec9f85e324c9cd9578",
+    "url": "./static/js/main.a523c4f1.chunk.js"
+  },
+  {
+    "revision": "3b5176513c78f0fdd135",
+    "url": "./static/js/runtime-main.03defe92.js"
+  }
+]);
